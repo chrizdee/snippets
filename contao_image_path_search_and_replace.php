c0a5998966cd934fa7ecaec6bd97646f01625674
chrizdee <cd@novo-online.de>
2013-08-07 09:53:00 +0200

    Changed code formate

--- contao_image_path_search_and_replace.php
+++ contao_image_path_search_and_replace.php
@@ -1,69 +1,69 @@
 <html>
 <head>
 	<title>Contao image path search and replace</title>
-<style type="text/css">
-body { font-family: sans-serif; font-size: 0.85rem; }
-table { border-collapse: collapse; }
-td { border: 1px solid #ccc; padding: 10px; vertical-align: top; }
-</style>
+    <style type="text/css">
+        body { font-family: sans-serif; font-size: 0.85rem; }
+        table { border-collapse: collapse; }
+        td { border: 1px solid #ccc; padding: 10px; vertical-align: top; }
+    </style>
 </head>
 <body>
-<h1>Contao image path search and replace</h1>
-<table>
-<?php
+    <h1>Contao image path search and replace</h1>
+    <table>
+    <?php
 
-// ----------
-$csv_source = 'export.csv';
-$search = 'tl_files/content/a/';
-$replace = 'tl_files/content/b/';
+    // ----------
+    $csv_source = 'export.csv';
+    $search = 'tl_files/content/a/';
+    $replace = 'tl_files/content/b/';
 
-// ----------
+    // ----------
 
 
-$row = 1;
-if (($handle = fopen($csv_source, "r")) !== FALSE) 
-{
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) 
+    $row = 1;
+    if (($handle = fopen($csv_source, "r")) !== FALSE) 
     {
-        $num = count($data);
-        $row++;
-        echo "<tr>";
-        for ($c=0; $c < $num; $c++) 
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) 
         {
-            if ($c==12)
+            $num = count($data);
+            $row++;
+            echo "<tr>";
+            for ($c=0; $c < $num; $c++) 
             {
-            	echo "<td class='col_" . $c . " original'>" . $data[$c] . "</td>";
-            	if ($data[$c])
-            	{
-	            	unset($images); $images = array();
-	            	foreach (unserialize($data[$c]) as $image) 
-	            	{
-	            		$image = str_replace($search, $replace, $image);	
-	            		array_push($images, $image);
-	            	}
-            	}            	
-            	echo "<td class='col_" . $c . "'>";
-            	print_r($images);
-            	echo "</td>";
-            	echo "<td class='col_" . $c . " serialized'>" . serialize($images) . "</td>";
+                if ($c==12)
+                {
+                	echo "<td class='col_" . $c . " original'>" . $data[$c] . "</td>";
+                	if ($data[$c])
+                	{
+    	            	unset($images); $images = array();
+    	            	foreach (unserialize($data[$c]) as $image) 
+    	            	{
+    	            		$image = str_replace($search, $replace, $image);	
+    	            		array_push($images, $image);
+    	            	}
+                	}            	
+                	echo "<td class='col_" . $c . "'>";
+                	print_r($images);
+                	echo "</td>";
+                	echo "<td class='col_" . $c . " serialized'>" . serialize($images) . "</td>";
 
-            	$out .='"'.serialize($images).'",';
+                	$out .='"'.serialize($images).'",';
+                }
+                else
+                {
+                	$data_mod = str_replace($search, $replace, $data[$c]);
+                	echo "<td class='col_" . $c . "'>" . $data_mod . "</td>";
+                	$out .='"'.$data_mod.'",';
+                }
             }
-            else
-            {
-            	$data_mod = str_replace($search, $replace, $data[$c]);
-            	echo "<td class='col_" . $c . "'>" . $data_mod . "</td>";
-            	$out .='"'.$data_mod.'",';
-            }
+            echo "</tr>";
+            $out = substr($out, 0, -1).chr(10);
         }
-        echo "</tr>";
-        $out = substr($out, 0, -1).chr(10);
+        fclose($handle);
     }
-    fclose($handle);
-}
-?>
-</table>
-<h2>CSV Output</h2>
-<textarea style="width: 100%; height: 500px;"><?php echo $out ?></textarea>
+    ?>
+    </table>
+    <h2>CSV Output</h2>
+    <textarea style="width: 100%; height: 500px;"><?php echo $out ?></textarea>
 </body>
 </html>
